c7a2ffd1dc454cd918227840e00f58217751aa34
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-05-03 20:23:40 +0200

    Update reload.py

--- scripts/reload.py
+++ scripts/reload.py
@@ -1,5 +1,11 @@
 import os
 
-os.system('systemctl daemon-reload')
-os.system('systemctl restart nginx')
-os.system('systemctl restart flaga.service')
+def reload(project_name)
+    os.system('systemctl daemon-reload')
+    os.system('systemctl restart nginx')
+    os.system('systemctl restart {}.service'.format(project_name)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        project_name = sys.argv[1]
+        reload(project_name)
