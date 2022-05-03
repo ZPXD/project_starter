d5249a316431f7b4b8fcba6028800d77c90f24fa
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-05-03 19:54:33 +0200

    Create remove_server_files.py

---
+++ scripts/remove_server_files.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('sudo rm /etc/nginx/sites-available/whatxdd.com')
+os.system('sudo rm /etc/nginx/sites-enabled/whatxdd.com')
+os.system('sudo rm /etc/systemd/system/what_what.service')
