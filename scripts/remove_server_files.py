5bfa60cda227eb64f3687881777cc28faacde5c4
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-05-03 20:25:49 +0200

    Update remove_server_files.py

--- scripts/remove_server_files.py
+++ scripts/remove_server_files.py
@@ -1,5 +1,13 @@
 import os
 
-os.system('sudo rm /etc/nginx/sites-available/whatxdd.com')
-os.system('sudo rm /etc/nginx/sites-enabled/whatxdd.com')
-os.system('sudo rm /etc/systemd/system/what_what.service')
+def remove_files(project_name, domain):
+    os.system('sudo rm /etc/nginx/sites-available/{}'.format(domain))
+    os.system('sudo rm /etc/nginx/sites-enabled/{}'.format(domain)
+    os.system('sudo rm /etc/systemd/system/{}.service'.format(project_name))
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        project_name = sys.argv[1]
+        project_name = sys.argv[2]
+        remove_files(project_name, domain)
+        print('done')
