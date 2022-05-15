6ee316fbfbbbd3b04e4292c37a90a410d81a9944
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-05-15 23:38:29 +0200

    Create terminal_print.py

---
+++ scripts/terminal_print.py
@@ -0,0 +1,26 @@
+import os
+
+'''
+In case if Flask output print act wierd and you are in a hurry.
+
+cp ?/project_starter/scripts/terminal_print.py where/your/script/is/terminal_print.py
+
+from terminal_print import tp
+tp('something')
+
+ZPXD, Łukasz Pintal.
+'''
+
+def tp(line):
+	with open('output.txt', 'w') as f:
+			f.write(line)
+			f.write('')
+	with open('output.txt', 'r') as f:
+		print(f.read())
+	os.system('cat output.txt')
+	print('\n')
+	os.system('rm output.txt')
+
+line = 'aaaaaaa'
+tp('aaaaaaa')
+
