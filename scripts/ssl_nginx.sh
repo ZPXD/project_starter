ef5139732a3d3617f6a810ddcf491d19a1edf173
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-05-21 12:15:47 +0200

    Update ssl_nginx.sh

--- scripts/ssl_nginx.sh
+++ scripts/ssl_nginx.sh
@@ -4,5 +4,5 @@
 domain=$1
 sudo apt update
 sudo apt install python3-certbot-nginx
-sudo pip install pyOpenSSL --upgrade
+sudo pip3 install pyOpenSSL --upgrade
 sudo certbot --nginx -d $domain -d www.$domain
